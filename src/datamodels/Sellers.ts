9182e34cfdfcb3d957ddfa2b48b6f319c006ed06
MartinCD <martincrisdarr@gmail.com>
2023-07-15 15:26:27 -0300

    primer commit

---
+++ src/datamodels/Sellers.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+
+export interface Seller {
+  name: string;
+  email: string;
+}
+
+
+const schema = new mongoose.Schema({
+  name: { type: String, required: true, index: true },
+  email: { type: String, required: true, index: true, unique: true }
+})
+
+export default mongoose.model<Seller>('Sellers', schema)
